a9413d070115333eacb3d65776f3e40f6934fbbd
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2018-04-21 10:00:32 +0700

    improvement vuetify trial

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <toolbar></toolbar>
+  </div>
+</template>
+
+<script>
+
+import Toolbar from './Toolbar.vue'
+
+export default {
+  components: {Toolbar},
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-toolbar color="blue" dark>
+    <v-toolbar-title class="white--text">Andriawan Website</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Link One</v-btn>
+      <v-btn flat>Link Two</v-btn>
+      <v-btn flat>Link Three</v-btn>
+    </v-toolbar-items>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
